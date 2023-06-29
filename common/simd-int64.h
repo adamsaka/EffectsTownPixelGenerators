883ec17931538ca9c16f19519317a6d4ae59cb6c
Maths Town <mathstownchannel@gmail.com>
2023-06-29 15:09:33 +1000

    Fix AVX Int64 multiplication

--- common/simd-int64.h
+++ common/simd-int64.h
@@ -392,7 +392,31 @@ struct Simd256Int64 {
 	Simd256Int64& operator-=(int64_t rhs) noexcept { v = _mm256_sub_epi64(v, _mm256_set1_epi64x(rhs));	return *this; }
 
 	//*****Multiplication Operators*****
-	Simd256Int64& operator*=(const Simd256Int64& rhs) noexcept { v = _mm256_mullo_epi64(v, rhs.v);	return *this; }
+	Simd256Int64& operator*=(const Simd256Int64& rhs) noexcept {
+		if constexpr (mt::environment::compiler_has_avx512vl && mt::environment::compiler_has_avx512dq) {
+			//There is a slight chance a user is using this struct but has compiled the code with avx512dq and avx512vl enabled.
+			this->v = _mm256_mullo_epi64(v, rhs.v);
+			return *this;
+		}
+		else {
+			//Multiplication (i64*i64->i64) is not implemented for AVX2.
+			//Algorithm:
+			//		If x*y = [a,b]*[c,d] (where a,b,c,d are digits/dwords).
+			//			   = (2^64) ac + (2^32) (ad + bc) + bd
+			//				 (We ignore upper 64bits so, we can ignore the (2^64) term.)
+			//
+			//Given lhs = [a,b] and rhs=[c,d]
+			auto digit1 = _mm256_mul_epu32(v, rhs.v);											//=[0+carry, bd]	: Calculate bd (i32*i32->i64)
+			auto rhs_swap = _mm256_shuffle_epi32(rhs.v, 0xB1);									//=[d,c]			: Swaps the low and high dwords on RHS . 
+			auto ad_bc = _mm256_mullo_epi32(v, rhs_swap);										//=[ad,bc]			: Multiply every dword together (i32*i32->i32).          
+			auto bc_00 = _mm256_slli_epi64(ad_bc, 32);											//=[bc,0]			: Shift Left to put bc in the upper dword.
+			auto ad_plus_bc = _mm256_add_epi32(ad_bc, bc_00);									//=[ad+bc,bc]		: Perofrm addition in the upper dword
+			auto digit2 = _mm256_and_si256(ad_plus_bc, _mm256_set1_epi64x(0xFFFFFFFF00000000)); //=[ad+bc,0]		: Zero lower dword using &
+			this->v = _mm256_add_epi64(digit1, digit2);											//=[ad+dc+carry,bd] : Add digits to get final result. 
+			return *this;
+		}
+
+	}
 	Simd256Int64& operator*=(int64_t rhs) noexcept { *this *= Simd256Int64(_mm256_set1_epi64x(rhs)); return *this; }
 
 	//*****Division Operators*****
